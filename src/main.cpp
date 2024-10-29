3c8a8a4f3b5d0495bfcfef80f6efbd7514697898
Pyhtagodzilla <1670671958@qq.com>
2024-10-29 12:16:31 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include<FastLED.h>
+
+
+#define NUM_LEDS 64
+#define DATA_PIN A0
+
+// put function declarations here:
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
