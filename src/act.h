6950db05dff3feb484e410c3fd5e78f261652941
Pyhtagodzilla <1670671958@qq.com>
2024-11-01 14:31:13 +0800

    maybe finished

--- src/act.h
+++ src/act.h
@@ -5,27 +5,173 @@
 #include <FastLED.h>
 #include "expression.h"
 
-void watchAround(){
-    display(watchCenter);
+int checkNum = 0;
+unsigned long lastCheck = 0;
+
+void colorfulScreen()
+{
+    for (int HSV_H = 0; HSV_H < 255; HSV_H++)
+    {
+        for (int light = 0; light < NUM_LEDS; light++)
+        {
+            leds[light] = CHSV(HSV_H, 255, 255);
+        }
+                    FastLED.show();
+            delay(25);
+            FastLED.clear();
+    }
+}
+
+void selfCheck()
+{
+    for (int lightNum = 0; lightNum < NUM_LEDS; lightNum++)
+    {
+        leds[lightNum] = CRGB::GreenYellow;
+    }
     FastLED.show();
-    delay(1000);
-    FastLED.clear(); 
-    
-    display(watchLeft);
-    FastLED.show();
-    delay(1000);
+    delay(500);
     FastLED.clear();
 
-    display(watchCenter);
+    for (int lightNum = 0; lightNum < NUM_LEDS; lightNum++)
+    {
+        leds[lightNum] = CRGB::White;
+    }
     FastLED.show();
-    delay(1000);
+    delay(500);
     FastLED.clear();
-    
-    display(watchRight);
+
+    displayWhite(tick);
     FastLED.show();
     delay(1000);
     FastLED.clear();
 }
 
+void rainbowLight()
+{
+    fill_rainbow(leds, 64, 0, 3);
+    FastLED.show();
+    delay(500);
+    FastLED.clear();
+}
+
+void watchLeftAndRight(){
+    displayWhite(watchCenter);
+    FastLED.show();
+    delay(100);
+    FastLED.clear(); 
+    
+    displayWhite(watchLeft);
+    FastLED.show();
+    delay(100);
+    FastLED.clear();
+
+    displayWhite(watchCenter);
+    FastLED.show();
+    delay(100);
+    FastLED.clear();
+    
+    displayWhite(watchRight);
+    FastLED.show();
+    delay(1000);
+    FastLED.clear();
+}
+
+void colorfulHeart(){
+    for (int colorSet = 0; colorSet < 255; colorSet++){
+        displayColor(heart,colorSet,255,255);
+        FastLED.show();
+    }
+}
+
+void switchFace(){
+    int randomNum = random(1,3);
+
+    switch(randomNum){
+        case 1:
+        watchLeftAndRight();
+        break;
+
+        case 2:
+        colorfulScreen();
+        break;
+        
+        case 3:
+        rainbowLight();
+        break;
+
+        case 4:
+        break;
+        }
+}
+
+void displayNumber(int num){
+    switch(num){
+        case 1:
+        FastLED.clear();
+        displayWhite(one);
+        FastLED.show();
+        break;
+
+        case 2:
+        FastLED.clear();
+        displayWhite(two);
+        FastLED.show();
+        break;
+
+        case 3:
+        FastLED.clear();
+        displayWhite(three);
+        FastLED.show();
+        break;
+
+        case 4:
+        FastLED.clear();
+        displayWhite(four);
+        FastLED.show();
+        break;
+
+        case 5:
+        FastLED.clear();
+        displayWhite(five);
+        FastLED.show();
+        break;
+
+        case 6:
+        FastLED.clear();
+        displayWhite(six);
+        FastLED.show();
+        break;
+
+        case 7:
+        FastLED.clear();
+        displayWhite(seven);
+        FastLED.show();
+        break;
+
+        case 8:
+        FastLED.clear();
+        displayWhite(eight);
+        FastLED.show();
+        break;
+
+        case 9:
+        FastLED.clear();
+        displayWhite(nine);
+        FastLED.show();
+        break;
+
+        default:
+        FastLED.clear();
+        displayWhite(wrong);
+        FastLED.show();
+        delay(1000);
+        displayWhite(zero);
+        FastLED.show();
+        checkNum = 0;
+        break;
+        }
+
+
+}
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,176 @@
 #include "expression.h"
 #include "act.h"
 
-//FastLED 初始化
+//Declaration of functions
+//void checkButton();
+void clickTick();
+
+//Button
+#define BUTTON A1
+#define DEBOUNCE 140
+
+byte buttonStatusCurrent = LOW;
+byte buttonStatusPrevious = LOW;
+//int checkNum = 0;
+//unsigned long lastCheck = 0;
+
+// FastLED 初始化
 #define NUM_LEDS 64
 #define DATA_PIN A0
 #define LED_TYPE WS2812
 #define COLOR_ORDER GRB
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  LEDS.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds,NUM_LEDS);
+  pinMode(BUTTON,INPUT_PULLUP);
+  Serial.begin(9600);
+
+  LEDS.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
   FastLED.setBrightness(max_bright);
 
-  watchAround();
+  pinMode(BUTTON, INPUT);//When pressed, turn to high
+  
+  selfCheck();
 
-  display(sigama);
+  /*colorfulHeart();
+
+  selfCheck();
+
+  rainbowLight();
+
+  watchLeftAndRight();
+
+  displayWhite(heart);
   FastLED.show();
   delay(1000);
   FastLED.clear();
+
+  displayWhite(sigama);
+  FastLED.show();
+  delay(1000);
+  FastLED.clear();
+
+  displayWhite(shock);
+  FastLED.show();
+  delay(1000);
+  FastLED.clear();
+
+  colorfulScreen();
+  FastLED.show();
+  delay(1000);
+  FastLED.clear();
+
+  displayWhite(shy);
+  FastLED.show();
+  delay(1000);
+  FastLED.clear();
+  Serial.println("shy finished");
+*/
+  delay(50);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  buttonStatusCurrent = digitalRead(BUTTON);
+  clickTick();
+  buttonStatusPrevious = buttonStatusCurrent;
 }
+
+
+void clickTick(){
+  static byte pressCount = 0;
+  static unsigned long clickMaxPeriod = 1000;
+  static unsigned long startTime = 0;
+  static bool isStart =false;
+
+  if ((buttonStatusCurrent == LOW) && (buttonStatusPrevious == HIGH)){
+    if (isStart == false){
+      pressCount = 1;
+      displayWhite(one);
+      FastLED.show();
+      delay(100);
+      isStart = true;
+      startTime = millis();
+    }else{
+      pressCount++;
+      displayNumber(pressCount);
+      startTime = millis();
+      
+      FastLED.show();
+      delay(100); 
+    }
+    delay(DEBOUNCE);
+  }
+
+  if (((millis() - startTime) > clickMaxPeriod) && isStart){
+    isStart = false;
+
+    switch (pressCount){
+      case 1:
+      colorfulHeart();
+      FastLED.clear();
+      break;
+
+      case 2:
+      watchLeftAndRight();
+      FastLED.clear();
+      break;
+
+      case 3:
+      rainbowLight();
+      FastLED.clear();
+      break;
+
+      case 4:
+      colorfulScreen();
+      FastLED.clear();
+      break;
+
+      case 5:
+      displayWhite(sigama);
+      FastLED.show();
+      break;
+       
+    }
+  }
+}
+
+/*void checkButton(){
+  int buttonStatus = digitalRead(BUTTON);
+
+  int nowTime = millis();
+
+  if (millis() - nowTime > 2000){
+    switch(checkNum){
+      case 1:
+      displayWhite(one);
+      case 2:
+      displayWhite(two);
+    }
+  }
+  
+  if (!buttonStatus){//判断按钮是否按下
+    delay(250);
+    if (!buttonStatus){//此时确定按钮被按下
+      if (millis() - lastCheck > 1000){//如果距离上次按下未超时,判断是否为第一次按下？？？
+      
+      Serial.println(checkNum);
+
+      checkNum = 1;
+
+      
+      lastCheck = millis();
+      }else{
+        lastCheck = millis();
+        checkNum++;
+        
+        Serial.println(checkNum);
+        
+        displayNumber(checkNum);
+      }
+    }
+  }
+}
+*/
